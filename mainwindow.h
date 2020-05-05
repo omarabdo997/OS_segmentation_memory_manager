a65a8b35bcbc72bc8223389cb19c11f5f05ecaf2
Omar <omarabdo997@gmail.com>
2020-05-05 06:36:17 +0200

    create main window gui

--- mainwindow.h
+++ mainwindow.h
@@ -1,9 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
 #include "segmentationselector.h"
 #include <QApplication>
+#include <QListWidgetItem>
+#include <QListWidget>
+#include <QList>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QTime>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,8 +20,27 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void draw();
+    void clear(QVBoxLayout *layout);
+    void delay();
+
+private slots:
+
+
+    void on_delete_process_clicked();
+
+    void on_add_process_clicked();
+
+protected:
+      void showEvent(QShowEvent *ev);
+
+
 
 private:
+    void showEventHelper();
     Ui::MainWindow *ui;
+    int number_processes=0;
+
+
 };
 #endif // MAINWINDOW_H
