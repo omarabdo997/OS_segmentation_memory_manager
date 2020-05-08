ce3fd57c41392f39fc35f79a8aa1adb8e135cafd
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2020-05-08 15:48:38 +0200

    Implement deallocate method

--- allocator.h
+++ allocator.h
@@ -5,9 +5,12 @@
 
 class Allocator
 {
+private:
+    void merge(int index, QVector<Segment>& segments,bool before);
 public:
     Allocator();
     virtual void allocate(Segment s, QVector<Segment>& segments)=0;
+    void deallocate(Segment s, QVector<Segment>& segments, QString pName);
     void deallocate(Segment s, QVector<Segment>& segments);
 };
 
